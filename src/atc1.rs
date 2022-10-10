e16a433add6914f1982fe9d70782ccff29b8a005
S-okubomy <okubo1504@gmail.com>
2022-10-10 14:04:23 +0900

    updated

--- src/atc1.rs
+++ src/atc1.rs
@@ -1,7 +1,7 @@
 use proconio::{ input };
 
 fn main() {
-    apple();
+    apple2();
 }
 
 #[allow(dead_code)]
@@ -11,4 +11,21 @@ fn apple() {
     }
 
     println!("{}", if 3 * x < y { n *x } else { (n/3) * y + (n%3 * x) });
+}
+
+#[allow(dead_code)]
+fn apple2() {
+    input! {
+        x: usize, y: usize, n: usize,
+    }
+
+    let mut min_money = 10000;
+    for j in 0..n {
+        if n < 3 * j { continue; }
+        let m =  x * (n - 3 * j) + y * j;
+        if min_money > m {
+            min_money = m;
+        }
+    }
+    println!("{}", min_money);
 }
