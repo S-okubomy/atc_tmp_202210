d79be6bf6f5b9083d041bf6618c7feee02c8bc65
S-okubomy <okubo1504@gmail.com>
2022-12-10 15:05:40 +0900

    upd_abc278_b_1

--- src/atc2.rs
+++ src/atc2.rs
@@ -1,16 +1,29 @@
 use itertools::Itertools;
 use proconio:: { input };
-use std::collections::{ VecDeque };
+use std::{collections::{ VecDeque }, fmt::format};
 
 fn main() {
     input! {
-        n: usize, k: usize,
-        a_vec: [usize; n],
+        mut h: usize, mut m: usize,
     }
-    let mut deque: VecDeque<usize> = VecDeque::from(a_vec);
-    for _ in 0..k {
-        deque.pop_front();
-        deque.push_back(0);
+    for _ in 0..24 {
+        for _ in 0..60 {
+            let (hs, ms) = (format!("{:0>2}", h), format!("{:0>2}", m));
+            let (tl, tr, dl, dr) = (&hs[0..=0], &ms[0..=0], &hs[1..=1], &ms[1..=1]);
+            let h_cnv: usize = format!("{:0>2}", tl.to_string() + tr).parse().ok().unwrap();
+            let m_cnv: usize = format!("{:0>2}", dl.to_string() + dr).parse().ok().unwrap();
+            if h_cnv <= 23 && m_cnv <=59 {
+                println!("{} {}", h, m);
+                return;
+            };
+            m += 1;
+            if m >= 60 {
+                m = 0;
+                h += 1;
+            }
+            if h >= 24 {
+                h = 0;
+            }
+        }
     }
-    println!("{}", deque.iter().map(|x| x.to_string()).join(" "));
 }
