fdb673e1383c375338582eca1948111eef91442f
S-okubomy <okubo1504@gmail.com>
2022-12-10 15:34:29 +0900

    upd_abc278_b_2

--- src/atc2.rs
+++ src/atc2.rs
@@ -6,24 +6,28 @@ fn main() {
     input! {
         mut h: usize, mut m: usize,
     }
-    for _ in 0..24 {
-        for _ in 0..60 {
-            let (hs, ms) = (format!("{:0>2}", h), format!("{:0>2}", m));
-            let (tl, tr, dl, dr) = (&hs[0..=0], &ms[0..=0], &hs[1..=1], &ms[1..=1]);
-            let h_cnv: usize = format!("{:0>2}", tl.to_string() + tr).parse().ok().unwrap();
-            let m_cnv: usize = format!("{:0>2}", dl.to_string() + dr).parse().ok().unwrap();
-            if h_cnv <= 23 && m_cnv <=59 {
-                println!("{} {}", h, m);
-                return;
-            };
-            m += 1;
-            if m >= 60 {
-                m = 0;
-                h += 1;
-            }
-            if h >= 24 {
-                h = 0;
-            }
+
+    while misjudge(h, m) {
+        m += 1;
+        if m == 60 {
+            m = 0;
+            h += 1;
+        }
+        if h == 24 {
+            h = 0;
         }
     }
+    println!("{} {}", h, m);
+}
+
+fn misjudge(h: usize, m: usize) -> bool {
+    let (a, b) = (h / 10, h % 10);
+    let (c, d) = (m / 10, m % 10);
+    let ac: usize = 10 * a + c;
+    let bd: usize = 10 * b + d;
+    is_out_24hours(ac, bd)
+}
+
+fn is_out_24hours(h: usize, m: usize) -> bool {
+    return !(h <= 23 && m <= 59);
 }
