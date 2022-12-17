53002ba7696498cd45b4e0e4c1cac383f711ab10
S-okubomy <okubo1504@gmail.com>
2022-12-17 21:46:41 +0900

    upd_abc282_c

--- src/atc0.rs
+++ src/atc0.rs
@@ -1,28 +1,24 @@
+use itertools::Itertools;
 use proconio::{ input };
 
 fn main() {
     input! {
-        n: usize, m: usize,
-        s: [String; n],
+        n: usize,
+        s: String,
     }
-    let s_vec: Vec<Vec<char>> = s.iter().map(|s| s.chars().collect()).collect();
-
-    let mut count = 0;
-    for i in 0..n {
-        for j in (i+1)..n {
-            if is_ok(&s_vec[i], &s_vec[j], m) {
-                count += 1;
-            }
+    let c_vec: Vec<char> = s.chars().collect();
+    let mut dq_cnt = 0;
+    let mut rep_c_vec: Vec<char> = Vec::new();
+    for c in c_vec {
+        if c == '"' {
+            dq_cnt += 1;
         }
-    }
-    println!("{}", count);
-}
-
-fn is_ok(p1: &Vec<char>, p2: &Vec<char>, m: usize) -> bool {
-    for i in 0..m {
-        if p1[i] == 'x' && p2[i] == 'x' {
-            return false;
+        let mut tmp_c = c;
+        if (dq_cnt % 2 == 0) && c == ','  {
+            tmp_c = '.'
         }
+        rep_c_vec.push(tmp_c);
     }
-    return true;
+
+    println!("{}", rep_c_vec.iter().collect::<String>());
 }
