dfd0a05e35be5cc06eb4d20646ae247c47fa2966
S-okubomy <okubo1504@gmail.com>
2022-12-18 13:45:49 +0900

    upd_abc_282_d_x

--- src/atc0.rs
+++ src/atc0.rs
@@ -3,22 +3,52 @@ use proconio::{ input };
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
-    }
-    let c_vec: Vec<char> = s.chars().collect();
-    let mut dq_cnt = 0;
-    let mut rep_c_vec: Vec<char> = Vec::new();
-    for c in c_vec {
-        if c == '"' {
-            dq_cnt += 1;
-        }
-        let mut tmp_c = c;
-        if (dq_cnt % 2 == 0) && c == ','  {
-            tmp_c = '.'
-        }
-        rep_c_vec.push(tmp_c);
+        n: usize, m: usize,
+        nv_vec: [(usize, usize); m],
     }
 
-    println!("{}", rep_c_vec.iter().collect::<String>());
+    let mut nb_vec: Vec<Vec<usize>> = vec![vec![]; n+1];
+    for nv in nv_vec {
+        let (n, v) = nv;
+        nb_vec[n].push(v);
+        nb_vec[v].push(n);
+    }
+
+
+// TODO 修正
+// https://atcoder.jp/contests/abc282/editorial/5397
+
+    let mut is_bipartite: bool = true;
+    let mut color: Vec<isize> = vec![-1; n+1];
+    let mut cnt = 0;
+    for i in 0..n {
+        if color[i] != -1 {
+            continue;
+        }
+        // if !dfs(&nb_vec, i, 0, &mut color) {
+        //     is_bipartite = false;
+        // }
+        if dfs(&nb_vec, i, 0, &mut color) {
+            cnt += 1;
+        }
+    }
+
+    println!("{}", cnt);
+}
+
+fn dfs(nb_vec: &Vec<Vec<usize>>, v: usize, cur: isize, color: &mut Vec<isize>) -> bool {
+    color[v] = cur;
+    for next_v in &nb_vec[v] {
+        if color[*next_v] != -1 {
+            if color[*next_v] == cur {
+                return false;
+            }
+            continue;
+        }
+
+        if !dfs(nb_vec, *next_v, 1 - cur, color) {
+            return false;
+        }
+    }
+    true
 }
