9899c04cc81c8133b0fa437f68368a09446c0e6d
S-okubomy <okubo1504@gmail.com>
2022-12-18 18:39:38 +0900

    upd_abc_280_d_x

--- src/atc2.rs
+++ src/atc2.rs
@@ -5,18 +5,29 @@ use proconio:: { input };
 
 fn main() {
     input! {
-        s: String,
-        t: String,
+        mut k: usize,
     }
-    let c_s_vec: Vec<char> = s.chars().collect();
-    let c_t_vec: Vec<char> = t.chars().collect();
 
-    let len = c_s_vec.len();
-    for i in 0..len {
-        if c_s_vec[i] != c_t_vec[i] {
-            println!("{}", i + 1);
-            return;
+    // TODO 修正
+    let k_copy = k;
+    let mut fact = 1;
+    let lim: usize = (k as f64).sqrt() as usize;
+    let mut fact_vec: Vec<(usize, usize)> = Vec::new();
+    for x in 2..=lim {
+        if k % x == 0 {
+            let mut cnt = 0;
+            while k % x == 0 {
+                cnt += 1;
+                k /= x;
+            }
+            fact_vec.push((x, cnt));
         }
     }
-    println!("{}", c_t_vec.len());
+
+    let len = fact_vec.len();
+    if len == 0 {
+        println!("{}", k_copy);
+    } else {
+        println!("{:?}", fact_vec[len-1].0);
+    }
 }
