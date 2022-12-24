a07a701769a3a9f5dbd00c993bebdc56e6275e7c
S-okubomy <okubo1504@gmail.com>
2022-12-24 22:26:54 +0900

    upd_abc283_c

--- src/atc0.rs
+++ src/atc0.rs
@@ -3,32 +3,18 @@ use proconio::{ input, marker::Chars };
 
 fn main() {
     input! {
-        n: usize,
-        mut a_vec: [usize; n],
-        q_size: usize,
+        s: Chars,
     }
 
-    let mut ans_vec: Vec<String> = Vec::new();
-    for _ in 0..q_size {
-        input! { q_type: usize, k: usize };
-        // println!("確認: {}", q_type);
-        if q_type == 1 {
-            input! { x: usize };
-            a_vec[k-1] = x;
-        } else if q_type == 2 {
-            ans_vec.push(a_vec[k-1].to_string());
+    let mut cnt = 0;
+    let mut is_renzoku = false;
+    for i in 0..s.len() - 1 {
+        if s[i] == '0' && s[i+1] == '0' && !is_renzoku {
+            cnt += 1;
+            is_renzoku = true;
+        } else {
+            is_renzoku = false;
         }
     }
-    println!("{}", ans_vec.join("\n"));
-
-    // for q in q_vec {
-    //     let qq = q.iter().map(|s| s.to_string()).collect::<Vec<String>>();
-    //     println!("{:?}", qq);
-    //     let k = qq[1].parse::<usize>().ok().unwrap();
-    //     if qq[0] == "1" {
-    //         a_vec[k-1] = qq[2].parse().ok().unwrap();
-    //     } else {
-    //         println!("{}", a_vec[k-1]);
-    //     }
-    // }
+    println!("{}", s.len() - cnt);
 }
