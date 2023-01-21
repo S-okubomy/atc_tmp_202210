c259c97f658e487ee6e3c48d5f7ac8adf9a4e265
S-okubomy <okubo1504@gmail.com>
2023-01-21 22:38:41 +0900

    upd_abc286_c_X

--- src/atc0.rs
+++ src/atc0.rs
@@ -3,9 +3,24 @@ use proconio::{ input, marker::Chars };
 
 fn main() {
     input! {
-        n: usize,
-        s: String,
+        n: usize, a: usize, b: usize,
+        s_vec: Chars,
     }
-    let ans: String = s.replace("na", "nya");
-    println!("{}", ans);
+
+    let mut sum = 0;
+    if s_vec[0] == s_vec[1] {
+        sum += a;
+        if n % 2 != 0 {
+            sum += (n - 3) / 2 * b;  
+        } else {
+            sum += (n - 2) / 2 * b;
+        }
+    } else {
+        if n % 2 != 0 {
+            sum += (n - 1) / 2 * b;  
+        } else {
+            sum += n / 2 * b;
+        }
+    }
+    println!("{}", sum);
 }
