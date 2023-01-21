09b68eab3e7bdc592118045f2b92521928f35ae3
S-okubomy <okubo1504@gmail.com>
2023-01-21 21:47:41 +0900

    upd_abc286_b

--- src/atc0.rs
+++ src/atc0.rs
@@ -3,24 +3,9 @@ use proconio::{ input, marker::Chars };
 
 fn main() {
     input! {
-        n: usize, p: usize, q: usize, r:usize, s: usize,
-        mut a_vec: [usize; n],
+        n: usize,
+        s: String,
     }
-    // let mut s1_vec: Vec<usize> = a_vec[r-1..s].to_vec();
-    // let mut s2_vec: Vec<usize> = a_vec[p-1..q].to_vec();
-    // let mut mid_vec: Vec<usize> = a_vec[q..r-1].to_vec();
-    // let mut last_vec: Vec<usize> = a_vec[s..n].to_vec();
-    // let mut ans: Vec<usize> = Vec::new();
-
-    // ans.append(&mut s1_vec);
-    // ans.append(&mut mid_vec);
-    // ans.append(&mut s2_vec);
-    // ans.append(&mut last_vec);
-    // println!("{}", ans.iter().join(" "));
-
-    let len = q - p + 1;
-    for i in 0..len {
-        a_vec.swap(p-1+i, r-1+i);
-    }
-    println!("{}", a_vec.iter().join(" "));
+    let ans: String = s.replace("na", "nya");
+    println!("{}", ans);
 }
